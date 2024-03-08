c2982229818391dbce871aed90c713ba47483547
Michael Clark <mc2.dev@proton.me>
2024-03-08 16:18:24 -0600

    added reading time

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 import mdx from "@astrojs/mdx";
 
@@ -22,6 +23,7 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [
       remarkToc,
+      remarkReadingTime,
       [
         remarkCollapse,
         {
