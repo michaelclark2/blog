265363834af80e81c21daaea44c7b4280f1f7f8b
Michael Clark <mc2.dev@proton.me>
2024-03-11 15:30:45 -0500

    update email

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const SOCIALS: SocialObjects = [
   },
   {
     name: "Mail",
-    href: "mc2.dev@proton.me",
+    href: "mailto:mc2.dev@proton.me",
     linkTitle: `Send an email to ${SITE.author}`,
     active: true,
   },
